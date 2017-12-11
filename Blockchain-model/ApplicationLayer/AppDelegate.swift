d071a7be3944610da20923888d4109e0549a9bc2
Nikita Kazakov <kazakov@magora-systems.com>
2017-12-11 11:33:00 +0700

    Initial commit.

---
+++ Blockchain-model/ApplicationLayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Blockchain-model
+//
+//  Created by Никита Сергеевич on 11.12.2017.
+//  Copyright © 2017 Никита Сергеевич. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Blockchain-model/Workflows/Main/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Blockchain-model
+//
+//  Created by Никита Сергеевич on 11.12.2017.
+//  Copyright © 2017 Никита Сергеевич. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
